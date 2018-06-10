7e8a900c4e0c6a22ea639f7e486c08283ae44221
Lucas Vieira Cardoso <lucas.v.cardoso@gmail.com>
2018-06-09 23:26:53 -0300

    Creation of project

---
+++ EmailObfuscator.Tests/EmailTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmailObfuscator;
+
+namespace EmailObfuscator
+{
+    public class EmailTests
+    {
+        [Test]
+        public void ObfuscateTest()
+        {
+            //Arrange
+            string email = $"name.surname@emailprovider.com";
+            string expected = $"*****surname@emailprovider.com";
+
+            //Act
+            string obfuscated = Email.Obfuscate(email);
+
+            //Assert
+            Assert.AreEqual(expected, obfuscated);
+        }
+    }
+}
---
+++ EmailObfuscator/Email.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EmailObfuscator
+{
+    public class Email
+    {
+        const int NUMBER_OF_ASTERISKS = 5;        
+
+        public static string Obfuscate(string emailAddress, int numberOfAsterisks = NUMBER_OF_ASTERISKS)
+        {
+            string obfuscatedEmail = "";
+
+            try
+            {
+                string[] parts = emailAddress.Split('@');
+                int endPosition = parts[0].Length <= numberOfAsterisks ? parts[0].Length / 2 : numberOfAsterisks;
+                string asterisks = new string('*', endPosition);
+
+                string primeiraParte = parts[0].Substring(endPosition, parts[0].Length - endPosition);
+                obfuscatedEmail = $"{asterisks}{primeiraParte}@{parts[1]}";
+            }
+            catch (Exception)
+            {
+                throw new FormatException($"Invalid e-mail format: {emailAddress}"); 
+            }
+
+            return obfuscatedEmail;
+        }
+    }
+}
